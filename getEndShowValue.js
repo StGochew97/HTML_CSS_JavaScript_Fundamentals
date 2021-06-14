d5f9707a18a1f3cf3e57036a477f56e9f16d65f9
StGochew97 <79480506+StGochew97@users.noreply.github.com>
2021-06-14 17:17:54 +0300

    Delete getEndShowValue.js

--- getEndShowValue.js
+++
@@ -1,9 +0,0 @@
-var inputNode  = document.querySelector("input[type='text']");
-
-var outputNode = document.querySelector("#output");
-
-function getContent( ){
-  outputNode.innerHTML = inputNode.value;
-}
-
-getContent();
